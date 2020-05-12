d5cdee424d520f496159a8b313c9056688c300b1
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-05-12 19:38:30 +0300

    app-test

--- src/test/java/com/mkyong/hashing/AppTest.java
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -1,13 +1,16 @@
 package com.mkyong.hashing;
 
-import junit.framework.Test;
+/*import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.TestSuite;*/
+import org.junit.Assert;
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+/*public class AppTest
     extends TestCase
 {
     /**
@@ -15,24 +18,38 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+/*    public AppTest( String testName )
     {
         super( testName );
-    }
-
+    }*/
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+  /*  public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
-
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
         assertTrue( true );
     }
+}*/
+
+
+
+public class AppTest {
+
+    private String INPUT = "123456";
+
+    @Test
+    public void testLength() {
+        Assert.assertEquals(64, App.sha256hex(INPUT).length());
+    }
+
+    @Test
+    public void testHex() {
+        String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+        Assert.assertEquals(expected, App.sha256hex(INPUT));
+    }
+
 }
