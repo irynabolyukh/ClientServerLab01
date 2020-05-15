8f8c37fcdbea49dcabf6e1f81c4a5df282ba5f09
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-05-15 19:18:30 +0300

    messageBody fixed

--- src/main/java/org/clientserver/Main.java
+++ src/main/java/org/clientserver/Main.java
@@ -1,5 +1,8 @@
 package org.clientserver;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -7,12 +10,12 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
-    public static void main(String[] args) throws BadPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException {
+    public static void main(String[] args) throws BadPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException, DecoderException {
         MessageCipher mc = new MessageCipher();
         final byte[] respond = Encode.buildResponse(mc);
         final String resp = new String(respond);
         System.out.println("Package to send: "+resp);
-       System.out.println("\nResponse from server:");
-       Decode.decode(Encode.buildResponse(mc),mc);
+        System.out.println("\nResponse from server:");
+        Decode.decode(Encode.buildResponse(mc),mc);
     }
 }
